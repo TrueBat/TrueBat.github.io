cbe53f9aaf016b9777d744511131817c9cf7dfd0
TrueBat <b4tb0ywalker@gmail.com>
2022-02-26 20:09:18 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const actions = Array.from(
+    document.querySelectorAll('[data-action]')
+);
+
+let score = localStorage.getItem('score') || 0;
+
+document.querySelector(".value").innerText = score;
+
+actions.forEach(action => {
+    action.addEventListener('click', () => {
+        const type = action.dataset.action;
+        console.log(type);
+        switch (type) {
+            case 'one':
+                score++;
+                alert("eh, do better next time");
+                break;
+            case 'two':
+                score++;
+                score++;
+                alert("GOOD JOB KEEP IT FUCKEN UP!!!");
+                break;
+            case 'zero':
+                alert("srsly...? anyway ur score was "+score);
+                score=0;
+                break;
+            case 'save':
+                localStorage.setItem('score', score);
+                alert('score have been saved!');
+                break;
+            case 'load':
+                score= localStorage.getItem('score');
+                break;
+            case 'clear':
+                let c = confirm("are you sure you want to clear your progress?");
+                if (c) {
+                    localStorage.clear();
+                    score = 0;
+                }
+                break;
+        }
+
+        document.querySelector(".value").innerText = score;
+
+    });
+});
---
+++ nya.java
@@ -0,0 +1,39 @@
+/*
+ * windy way
+ * 1. 50 push ups .5
+ * 2. 20 chin ups .5
+ * 3. abs workout .5  
+ * 4. legs workout .5
+ * 5. 20min typing .5
+ * 6. python .5
+ * 7. JavaScript .5
+ * 8. work on story .5
+ * 9. 3 pages book .5
+ * 10. learn unity/unreal .5
+ * 11. c# .5
+ * 12. c++ .5
+ * 13. learn VS code shortcuts .5
+ * 14. 20 weighted pushups .5 // 
+ * 15. learn more CSS .5
+ * 16. new DC rules .5
+ * 17. come up with a programming project idea .5
+ * 
+ * 
+ * o-o-o-o-o:
+ * 1. learn vs shortcuts
+ * 2. story
+ * 3. CSS
+ * 
+ * 
+ * 
+ * 
+ * pnts:
+ * 
+ * myst:.5
+ * bat:.5
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
